795a4f8e271def08dd56d6a72ba91040425b5b49
djbartos93 <djbartos93@gmail.com>
2018-02-08 09:32:16 -0600

    Revert "Revert "add punchblock scaffold""
    This reverts commit e0ea0f0a12446b6ff3ee10af27400c0a67c1dac5.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-=======
-  resources :terminals
-  resources :wires
->>>>>>> parent of f0e3da0... add punchblock scaffold
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
